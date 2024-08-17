11f57301a870062facd60ec8b37efa26d34e4fc1
davidzherd <dzherdenovsky+github@gmail.com>
2024-08-17 15:23:22 +0300

    test

--- src/Context.ts
+++ src/Context.ts
@@ -3,5 +3,5 @@ import { createContext } from "react";
 export const GalleryContext = createContext({
   currentGalleryTitle: "",
   currentGalleryId: "",
-  changeGallery: (gallery: { title: string; id: string }) => {},
+  changeGallery: (_gallery: { title: string; id: string }) => {},
 });
